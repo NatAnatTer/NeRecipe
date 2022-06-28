7365756347a8695fa937c3792135e93a24fd2f7a
Natalia <natinho@yandex.ru>
2022-06-28 22:22:23 +0300

    ci

--- app/src/main/java/ru/netology/nerecipe/ui/FilterFragment.kt
+++ app/src/main/java/ru/netology/nerecipe/ui/FilterFragment.kt
@@ -24,7 +24,7 @@ class FilterFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = FilterFragmentBinding.inflate(inflater, container, false)
         .also { binding ->
-            var flag = false
+            var flag = true
             binding.cbSelectAll.isChecked = flag
             val adapter = FilterAdapter(viewModel, flag)
             binding.categoryListRecyclerView.adapter = adapter
